78bae657c58959894c839a4a1be541b02a3bfcac
rabby-shek <rabbyshek1998@gmail.com>
2024-08-08 00:00:04 +0600

    Not found page complete

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      Not Found
-    </div>
-  )
-}
+    <section className="py-3 py-md-5 min-vh-100 d-flex justify-content-center align-items-center">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="text-center">
+              <h2 className="d-flex justify-content-center align-items-center gap-2 mb-4">
+                <span className="display-1 fw-bold">404</span>
+              </h2>
+              <h3 className="h2 mb-2">Oops! You're lost.</h3>
+              <p className="mb-5">
+                The page you are looking for was not found.
+              </p>
+              <button
+                className="btn bsb-btn-5xl btn-dark rounded-pill px-5 fs-6 m-0"
+                onClick={() => navigate(-1)}
+              >
+                Back to Home
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default NotFound;
